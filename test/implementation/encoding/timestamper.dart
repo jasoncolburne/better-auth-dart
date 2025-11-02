155c4e27d07caf4cc33aa5b5f4e2c815e2a00f55
Jason Colburne <j.colburne@gmail.com>
2025-11-02 12:26:16 -0400

    millisecond precision

--- test/implementation/encoding/timestamper.dart
+++ test/implementation/encoding/timestamper.dart
@@ -1,12 +1,16 @@
 import 'package:better_auth_dart/interfaces/encoding.dart';
 
-class Rfc3339Nano implements ITimestamper {
+class Rfc3339 implements ITimestamper {
   @override
   String format(DateTime when) {
     // Use standard RFC3339 format with millisecond precision (3 digits)
     final isoString = when.toUtc().toIso8601String();
-    // Dart's toIso8601String() already uses millisecond precision by default
-    return isoString;
+    // Dart's toIso8601String() produces microsecond precision (6 digits)
+    // Truncate to milliseconds (3 digits) using regex
+    return isoString.replaceFirstMapped(
+      RegExp(r'\.(\d{3})\d+Z$'),
+      (match) => '.${match.group(1)}Z',
+    );
   }
 
   @override
